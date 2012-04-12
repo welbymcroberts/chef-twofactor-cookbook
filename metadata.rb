d5affdf527a2a3cf2aabdd835e509e9caa550a29
Welby McRoberts <welby@whmcr.com>
2012-04-12 15:53:44 +0100

    Inital commit - not quite working

---
+++ metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "Welby McRoberts"
+maintainer_email "welby@whmcr.com"
+license          "All rights reserved"
+description      "Installs/Configures yubikey"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.2"
---
+++ recipes/default.rb
@@ -0,0 +1,36 @@
+#
+# Cookbook Name:: yubikey
+# Recipe:: default
+#
+# Copyright 2012, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+
+
+package "pam_yubico" do
+  package_name "pam_yubico"
+  not_if "rpm -qa | egrep 'pam_yubico'"
+  action :install
+end
+
+yk = []
+begin
+    data_bag("admins").each do |login|
+        dbi = data_bag_item('admins',login)
+        yk.push [dbi['username'],dbi['yubikey']]
+    end
+rescue
+    Chef::Log.info "no users databag"
+end
+print yk
+
+template "/etc/yubikeys" do
+  source "yubikeys.erb"
+  variables(
+   :yubikeys => yk
+  )
+end
+
+template "/etc/pam.d/sshd" do
+   source "sshd.erb"
+end
